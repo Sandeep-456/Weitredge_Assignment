45c13db054c553e9ba46921a56702da32c0c5ba2
Indugula.Sandeep <sandeepindugula456@gmail.com>
2026-02-24 22:18:23 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -18,11 +18,15 @@ app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(new Error("CORS policy violation"), false);
+      
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Blocked Origin:", origin); 
+        callback(new Error("CORS policy violation"), false);
       }
-      return callback(null, true);
     },
+    credentials: true,
   }),
 );
 
